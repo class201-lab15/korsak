e84da178ae6e0dd15a2f5d205fb907e23f504f2b
haneenzyad98 <haneenzyad98@gmail.com>
2021-03-02 03:42:35 +0200

    home page

--- secand.js
+++ secand.js
@@ -1,16 +1,24 @@
-window.onscroll = function() {myFunction()};
+var modal = document.getElementById("myModal");
 
-// Get the navbar
-var navbar = document.getElementsByClassName("topnav")
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
